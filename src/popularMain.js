1311ef530b2711448546ca4a12e26af6f30c361c
broto1234 <satyo97du@gmail.com>
2025-05-06 21:50:10 +0200

    last update

--- src/popularMain.js
+++ src/popularMain.js
@@ -1,16 +1,14 @@
 import './styles/style.scss';
-import { getMostPopular } from './views/conponents/popular';
 
-const newsArticles = getMostPopular('viewed', 7);
-articleSearch();
-console.log(newsArticles);
+// import { getMostPopular } from './views/conponents/popular';
+// const newsArticles = getMostPopular('viewed', 7);
+// articleSearch();
+// console.log(newsArticles);
+// document.querySelector('#app').innerHTML = ``
 
+import header from './views/conponents/header';
+import popular from './views/conponents/popular';
+import footer from './views/conponents/footer';
 
-document.querySelector('#app').innerHTML = ``
-
-// import header from './views/conponents/header';
-// import popular from './views/conponents/popular';
-// import footer from './views/conponents/footer';
-
-// const divElm = document.querySelector('#app');
-// divElm.append(header(),popular(),footer());
+const divElm = document.querySelector('#app');
+divElm.append(header(),popular(),footer());
